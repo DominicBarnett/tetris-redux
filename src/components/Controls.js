c9fd3dba03cc81eb4db9ae10be9329133ccf5780
DominicBarnett <dominic.barnett@icloud.com>
2025-04-24 15:49:56 -0700

    timer created

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice';
 
@@ -6,7 +6,37 @@ import { moveDown, moveLeft, moveRight, rotate } from '../features/gameSlice';
 
 export default function Controls() {
   const dispatch = useDispatch();
-  const { isRunning } = useSelector((state) => state);
+  const { isRunning, speed } = useSelector((state) => state);
+
+  // Track animation frame timing
+  const requestRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
+
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+  
+    if (!isRunning) return;
+  
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+  
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+  
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
+  
+    lastUpdateTimeRef.current = time;
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isRunning]);
 
   return (
     <div className="controls">
